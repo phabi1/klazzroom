21a67730def7c6be124e2882bf5b7778036dabc5
Fabien Heilles <phabi1@hotmail.fr>
2023-12-08 08:09:30 +0100

    create timetable microservice

---
+++ apps/server/timetable/src/main.ts
@@ -0,0 +1,23 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = '';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  const host = process.env.HOST || 'localhost';
+  await app.listen(port, host);
+  Logger.log(
+    `🚀 Application is running on: http://${host}:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
